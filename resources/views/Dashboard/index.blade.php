6bd618c04aeea1f089c6b9212595652340e8efba
mochammadfiqri <fiqri177@gmail.com>
2023-02-20 16:07:19 +0700

    Edit Preloader

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -3,5 +3,9 @@
 @section('judul', 'Dashboard')
 
 @section('content')
+      <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{ asset('assets/AdminLTE/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+  </div>
     @include('Dashboard.html')
 @endsection
--- resources/views/layouts/backend-dashboard/app.blade.php
+++ resources/views/layouts/backend-dashboard/app.blade.php
@@ -13,10 +13,10 @@
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
-  <!-- Preloader -->
+  {{-- <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
     <img class="animation__shake" src="{{ asset('assets/AdminLTE/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
-  </div>
+  </div> --}}
 
   <!-- Navbar -->
   @include('layouts.backend-dashboard.navbar')
